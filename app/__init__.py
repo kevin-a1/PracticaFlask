1c0d8ee53adab850cb6d82a98ab6a42eb739e4b1
kevin-a1 <kevin.llivichuzhca.est@gmail.com>
2021-02-24 14:13:34 -0500

    casi ultimo

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from flask import render_template,request, redirect
+import boto3
+import uuid
+import requests
+import botocore
+
+from botocore import UNSIGNED
+from botocore.client import Config
+from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 
+
+bucket = 'examenllivichuzhcakevin'
+s3 = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='')
+s3._request_signer.sign = (lambda *args, **kwargs: None)
+
 @app.route('/' ,methods=["GET", "POST"])
 def index():
+    imagen = None
     if request.method == 'POST':
-     imagen = request.form['imagen']
-     print(imagen)
-    return render_template("index.html")
+     imagen = request.files['imagen']
+     s3.upload_fileobj(imagen,bucket,'img/image1.png', ExtraArgs={'ACL':'public-read'})
+
+    return render_template("index.html",imag = imagen)
