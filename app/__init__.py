85a6d548b50d497f16d6ef7c191562e33d918c3f
kevin-a1 <kevin.llivichuzhca.est@gmail.com>
2021-02-24 13:16:42 -0500

    examen

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hola Kevin'
-
-posts = []
-@app.route('/inicio')
-@app.route('/inicio/<string:slug>/')
-def index(slug = 'Usuario'):
-    return render_template("index.html", num_posts = slug)
+@app.route('/' ,methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+     imagen = request.form['imagen']
+     print(imagen)
+    return render_template("index.html")
